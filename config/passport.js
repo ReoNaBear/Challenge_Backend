36ef8cc14674b0f6ec94ffc5eac3396cf6a9644c
ReoNaBear <89585478+ReoNaBear@users.noreply.github.com>
2023-01-02 15:50:39 +0800

    feat: remove passport localStrategy & add gitignore

--- config/passport.js
+++ config/passport.js
@@ -1,39 +1,12 @@
 const passport = require('passport')
-
-const LocalStrategy = require('passport-local')
 const passportJWT = require('passport-jwt')
-const bcrypt = require('bcryptjs')
 const { User, UserAuth } = require('../models')
 const JWTStrategy = passportJWT.Strategy
 const ExtractJWT = passportJWT.ExtractJwt
 
-passport.use(
-  new LocalStrategy(
-    {
-      usernameField: 'account',
-      passwordField: 'password',
-      passReqToCallback: true
-    },
-    (req, account, password, cb) => {
-      UserAuth.findOne({ where: { account } }).then(userAuth => {
-        if (!userAuth)
-          return cb(
-            null,
-            false,
-            req.flash('error_messages', 'Incorrect Account or Password!')
-          )
-        if (!bcrypt.compareSync(password, userAuth.password))
-          return cb(null, false, req.flash('error_messages', 'Incorrect Account or Password!'))
-        return cb(null, userAuth)
-      })
-    }
-  )
-)
-
 let jwtOptions = {}
 jwtOptions.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken()
 jwtOptions.secretOrKey = process.env.JWT_SECRET
-
 passport.use(new JWTStrategy(jwtOptions, async (jwtPayload, done) => {
   try {
     console.log(jwtPayload.userAuthId);
@@ -46,7 +19,7 @@ passport.use(new JWTStrategy(jwtOptions, async (jwtPayload, done) => {
       return done(null, false)
     }
     console.log(user);
-    return done(null, user)
+    done(null, user)
   } catch (err) {
     console.warn(err)
   }
