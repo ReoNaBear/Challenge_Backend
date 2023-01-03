4826e4eccbd97c6449b3d849234dacbea664ae45
ReoNaBear <89585478+ReoNaBear@users.noreply.github.com>
2023-01-03 23:03:07 +0800

    feat: add QRcodeAuth migration

---
+++ migrations/20230103082553-create-q-rcode-auth.js
@@ -0,0 +1,29 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('QRcodeAuths', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      companyCode: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      secretCode: {
+        allowNull: false,
+        type: Sequelize.UUID
+      },
+      updatedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('QRcodeAuths');
+  }
+};
