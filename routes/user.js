ec198e1bf7c7141c91aebdad4300b9804cd1f2a8
ReoNaBear <89585478+ReoNaBear@users.noreply.github.com>
2023-01-03 23:01:08 +0800

    feat: change authentication in user.js

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,12 @@ const router = express.Router()
 const userController = require('../controllers/user-controller')
 const { authenticated, authenticatedUser } = require('../middleware/api-auth')
 
-router.use(authenticated, authenticatedUser)
+router.use(authenticated)
+router.get('/current_user', userController.getCurrentUser)
 
+router.use(authenticatedUser)
 router.put('/account/password', userController.putPassword)
 
-router.get('/current_user', userController.getCurrentUser)
 
 
 module.exports = router
